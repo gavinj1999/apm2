98404a5a460e97b2e4e513b5ab5eb2d5c2de831e
gavinj1999 <gavinj2006@googlemail.com>
2025-06-14 05:37:33 +0100

    line

--- resources/js/pages/Activities/Index.vue
+++ resources/js/pages/Activities/Index.vue
@@ -7,7 +7,7 @@
       </h2>
     </template>
     <div class="py-12">
-      <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+      <div class="mx-auto sm:px-6 lg:px-8">
         <div class="bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
           <div class="p-6 bg-gray-800 border-b border-gray-700">
             <!-- Flash Message -->
